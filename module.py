8c3212923fdab89277c063d11a8a19ff36a44dec
ioan beilic <ioan@ioans-MacBook-Pro.local>
2026-02-24 00:24:10 +0100

    fix: remove leading indentation from module.py

--- module.py
+++ module.py
@@ -1,31 +1,31 @@
-    from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
-    MODULE_ID = 'property_mgmt'
-    MODULE_NAME = _('Property Management')
-    MODULE_VERSION = '1.0.0'
-    MODULE_ICON = 'home-outline'
-    MODULE_DESCRIPTION = _('Property listings, tenants and lease management')
-    MODULE_AUTHOR = 'ERPlora'
-    MODULE_CATEGORY = 'specialized'
+MODULE_ID = 'property_mgmt'
+MODULE_NAME = _('Property Management')
+MODULE_VERSION = '1.0.0'
+MODULE_ICON = 'home-outline'
+MODULE_DESCRIPTION = _('Property listings, tenants and lease management')
+MODULE_AUTHOR = 'ERPlora'
+MODULE_CATEGORY = 'specialized'
 
-    MENU = {
-        'label': _('Property Management'),
-        'icon': 'home-outline',
-        'order': 92,
-    }
+MENU = {
+    'label': _('Property Management'),
+    'icon': 'home-outline',
+    'order': 92,
+}
 
-    NAVIGATION = [
-        {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
+NAVIGATION = [
+    {'label': _('Dashboard'), 'icon': 'speedometer-outline', 'id': 'dashboard'},
 {'label': _('Properties'), 'icon': 'home-outline', 'id': 'properties'},
 {'label': _('Tenants'), 'icon': 'people-outline', 'id': 'tenants'},
 {'label': _('Leases'), 'icon': 'document-text-outline', 'id': 'leases'},
 {'label': _('Settings'), 'icon': 'settings-outline', 'id': 'settings'},
-    ]
+]
 
-    DEPENDENCIES = []
+DEPENDENCIES = []
 
-    PERMISSIONS = [
-        'property_mgmt.view_property',
+PERMISSIONS = [
+    'property_mgmt.view_property',
 'property_mgmt.add_property',
 'property_mgmt.change_property',
 'property_mgmt.delete_property',
@@ -36,4 +36,4 @@
 'property_mgmt.add_lease',
 'property_mgmt.change_lease',
 'property_mgmt.manage_settings',
-    ]
+]
